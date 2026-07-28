9ca69373188553a79ff48e59969b0c1445ea8152
Robert King <varitonium@gmail.com>
2026-07-28 23:41:09 +0200

    OBN-67: upe-amending of benchmarking sequence scenario

--- benchmark/sequence.cpp
+++ benchmark/sequence.cpp
@@ -6,6 +6,8 @@
 MICRO_CLOCK_RUNTIME_MAIN {
   clock();
   //quality check to gather picture metric 
+  //argv[1] input path
+  //argv[2] output path
   clock();
   fmt::println("time elasped: {}", clock.elapsed());
   return 0;
