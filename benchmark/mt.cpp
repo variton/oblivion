1ca19673405c590eacf7cefd844ec8a483c9ba7a
Robert King <varitonium@gmail.com>
2026-08-15 22:57:35 +0200

    OBN-68: dev-adding of benchmark multithreaded processing

---
+++ benchmark/mt.cpp
@@ -0,0 +1,15 @@
+
+#include <mgr.h>
+#include <fmt/core.h>
+#include <runtime.h>
+
+MICRO_CLOCK_RUNTIME_MAIN {
+  clock();
+  //quality check to gather picture metric 
+  //argv[1] input path
+  //argv[2] output path
+  //multithreaded approach
+  clock();
+  fmt::println("time elasped: {}", clock.elapsed());
+  return 0;
+}
