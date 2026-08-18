c57c4657c6dc7115a8745465ad2168b1ff22a5df
Robert King <varitonium@gmail.com>
2026-08-18 23:11:19 +0200

    OBN-70: upe-adding of extra benchmarking information

--- benchmark/sequence.cpp
+++ benchmark/sequence.cpp
@@ -7,7 +7,7 @@ MICRO_CLOCK_RUNTIME_MAIN {
   clock();
   //quality check to gather picture metric 
   //argv[1] input path
-  //argv[2] output path
+  //argv[2] output path pnsr ssim
   clock();
   fmt::println("time elasped: {}", clock.elapsed());
   return 0;
