32e65f887260eab7850b3aa2bf1a29dde9e00127
Robert King <varitonium@gmail.com>
2026-08-27 00:45:40 +0200

    OBN-82: dev-impl unit test ChannelImgDimError

--- src/img/test/test_channel_mgr.cpp
+++ src/img/test/test_channel_mgr.cpp
@@ -34,8 +34,24 @@ TEST_CASE("SplitRGBChannels OK") {
 }
 
 TEST_CASE("Channel invalid dimension") {
-  // prepare context to propagate the right error
-  CHECK(true);
+  std::string rc{"/home/cxx-core/rc"};
+  using namespace img;
+  std::string input{rc + "/ur.jpg"};
+  auto input_file = fio::FileHandler::open(input.data(), "rb");
+  // jpeg decompression creation ============================================
+  JpegDecompressor decompressor{};
+  auto ret_init_decompressor = decompressor.init(input_file.value().get());
+  auto ret_decompress = decompressor.decompress();
+  // image object creation ==================================================
+  InputImg inputimg{decompressor.cinfo()};
+  // jpeg decoder ===========================================================
+  JpegDecoder original{inputimg.width, inputimg.height, inputimg.components};
+  auto ret_decode = original.decode(decompressor);
+  CHECK(ret_decode.has_value());
+  auto pixels = original.pixels();
+  ChannelMgr channel_mgr{inputimg.width, -1, inputimg.components};
+  auto ret_split = channel_mgr.splitRGBChannels(pixels);
+  CHECK(ret_split.error().message == "Invalid image dimensions");
 }
 
 TEST_CASE("Channel expected rgb image") {
