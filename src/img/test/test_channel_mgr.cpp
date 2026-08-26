d6a7d9286a6b7c0003db6c3f010582e44f78a6bc
Robert King <varitonium@gmail.com>
2026-08-27 00:55:52 +0200

    OBN-83: upe-adding of unit test ChannelStructError

--- src/img/test/test_channel_mgr.cpp
+++ src/img/test/test_channel_mgr.cpp
@@ -55,6 +55,23 @@ TEST_CASE("Channel invalid dimension") {
 }
 
 TEST_CASE("Channel expected rgb image") {
-  // prepare context to propagate the right error
+  std::string rc{"/home/cxx-core/rc"};
+  using namespace img;
+  std::string input{rc + "/ur.jpg"};
+  auto input_file = fio::FileHandler::open(input.data(), "rb");
+  // jpeg decompression creation ============================================
+  JpegDecompressor decompressor{};
+  auto ret_init_decompressor = decompressor.init(input_file.value().get());
+  auto ret_decompress = decompressor.decompress();
+  // image object creation ==================================================
+  InputImg inputimg{decompressor.cinfo()};
+  // jpeg decoder ===========================================================
+  JpegDecoder original{inputimg.width, inputimg.height, inputimg.components};
+  auto ret_decode = original.decode(decompressor);
+  CHECK(ret_decode.has_value());
+  auto pixels = original.pixels();
+  ChannelMgr channel_mgr{inputimg.width, inputimg.height, 2};
+  auto ret_split = channel_mgr.splitRGBChannels(pixels);
+  CHECK(ret_split.error().message == "Expected a three-channel RGB image");
   CHECK(true);
 }
