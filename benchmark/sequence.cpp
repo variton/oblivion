bbfdc47709d7fe0c99dd27041339ea1efb7f7a29
Robert King <varitonium@gmail.com>
2026-08-30 23:12:37 +0200

    OBN-88: upe-adding of benchmark context

--- benchmark/sequence.cpp
+++ benchmark/sequence.cpp
@@ -3,12 +3,16 @@
 #include <fmt/core.h>
 #include <runtime.h>
 
+//#include <pixel_mgr.h>
+
 MICRO_CLOCK_RUNTIME_MAIN {
   clock();
+  //todo integrate pixel_mgr & mgr
+  fmt::println("{}",argv[1]);
   //quality check to gather picture metric 
   //argv[1] input path
   //argv[2] output path pnsr ssim
   clock();
-  fmt::println("time elasped: {}", clock.elapsed());
+  fmt::println("time elasped: {} us", clock.elapsed());
   return 0;
 }
